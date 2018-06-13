dc519d857d82d4b14a4d41a678622507b41118d4
Edward Brennan <EdMan1022@gmail.com>
2018-06-13 12:52:39 -0400

    Add some tests for a Session class

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -20,3 +20,31 @@ class TestAuth(BaseTestClass):
     def test_too_short_expires_in(self):
         with self.assertRaises(ValueError):
             Auth(self.access_token, self.token_type, 10, self.scope)
+
+
+class TestSession(BaseTestClass):
+    target_path = 'pyketo.session'
+
+    def setUp(self):
+        self.requests_patch = self.create_patch('requests')
+
+        self.test_identity_url = 'test'
+        self.test_client_id = 'test'
+        self.test_client_secret = 'test'
+
+        self.session = Session(
+            self.test_identity_url,
+            self.test_client_id,
+            self.test_client_secret
+        )
+
+    def test_init(self):
+        self.assertIsInstance(self.session, Session)
+
+    def test_refresh_auth_token(self):
+        old_token = self.session.auth.token
+        old_token_created = self.session.auth.created_time
+        self.session.refresh_auth_token()
+        new_token = self.session.auth.token
+
+        self.assertNotEqual(old_token, new_token)
