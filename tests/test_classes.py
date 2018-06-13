aebf10bb10a781a6b275a42d058febbaec2ff059
Edward Brennan <EdMan1022@gmail.com>
2018-06-13 12:47:19 -0400

    Create tests for Auth class

---
+++ tests/test_classes.py
@@ -0,0 +1,20 @@
+from . import BaseTestClass
+
+
+class TestAuth(BaseTestClass):
+
+    target_path = 'pyketo.auth'
+
+    def setUp(self):
+        self.access_token = 'test'
+        self.token_type = 'test'
+        self.expires_in = 40
+        self.scope = 'test'
+
+    def test_init(self):
+        auth = Auth(self.access_token, self.token_type, self.expires_in,
+                    self.scope)
+
+    def test_too_short_expires_in(self):
+        with self.assertRaises(ValueError):
+            Auth(self.access_token, self.token_type, 10, self.scope)
