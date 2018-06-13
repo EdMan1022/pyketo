4c69d80d4b4e3f7f4ca37f88444dfd68957949b5
Edward Brennan <EdMan1022@gmail.com>
2018-06-13 12:45:59 -0400

    Create pypi package skeleton

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as file_obj:
+    long_description = file_obj.read()
+
+packages = setuptools.find_packages()
+
+setuptools.setup(
+    name='Pyketo',
+    version='0.1dev',
+    author="Edward Brennan",
+    author_email="EdMan1022@gmail.com",
+    description="A Python implementation of the Marketo API",
+    long_description=long_description,
+    url="https://github.com/EdMan1022/pyketo.git",
+    packages=packages,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
